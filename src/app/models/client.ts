c0fbe0e6cda90ba512783fb920e7e536757f0844
Ndoda Kheswa (ZA) <Ndoda.Kheswa@absa.africa>
2020-03-26 00:22:40 +0200

    added client model and material module

---
+++ src/app/models/client.ts
@@ -0,0 +1,11 @@
+export class {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  accupation: string;
+  gender: string;
+  birthDate: Date;
+  age: number;
+}
