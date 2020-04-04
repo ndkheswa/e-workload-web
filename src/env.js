bf1741d802d7fff7e258c6699428c245c498feb2
Ndoda Kheswa (ZA) <Ndoda.Kheswa@absa.africa>
2020-04-04 20:48:41 +0200

    adding env.js

---
+++ src/env.js
@@ -0,0 +1,11 @@
+(function (window) {
+  window.__env = window.env || {};
+
+  //api url
+  window.__env.apiRoot = '';
+  window.__env.clientId = '';
+  window.__env.oktaDomain = '';
+  window.__env.oktaUrl = '';
+  window.__env.enableDebug = false;
+
+} (this));
